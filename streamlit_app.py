42f1c2623a9b55b47789c931ad7496f94649f5e2
Austin Choi <38763311+CAholder@users.noreply.github.com>
2024-07-21 16:11:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,8 @@ def create_sorted_bar_chart_brand_type(data, sort_by, title):
 
 def answer_question(df, question):
     question = question.lower()
-    api = st.secrets["anthropic"]
-    client = Anthropic(api_key=api)
+    os.environ["ANTHROPIC_API_KEY"] = st.secrets["anthropic"]
+    client = Anthropic()
     MODEL_NAME = "claude-3-5-sonnet-20240620"
     glasses_df = load_data()
     csv_content = glasses_df.to_string(index=False)
